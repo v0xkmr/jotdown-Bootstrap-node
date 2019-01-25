231020dadc500596f61be75a7e4072b1b4117e6f
vijay kumar <vijaykumar0737@gmail.com>
2019-01-25 14:26:48 +0530

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const { post } = require('./routes/post');
 const { customer } = require('./routes/customer');
 
 const app = express();
+app.use(cors())
 
 mongoose.connect('mongodb+srv://vijay:vijay@mongocluster-bjhe8.mongodb.net/jotdownBootstrap?retryWrites=true')
     .then(() => { console.log('Connected to MongoDB'); })
